292d9e8f2015305a6368ce722c90aa1af83d61ec
Mostafa Elfishawy <fisho_world@yahoo.com>
2018-12-28 19:43:52 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,14 @@
 def using_until
+  
   levitation_force = 6
   
-  untill levitation_force <= 10
-  puts "Wingardium Leviosa"
+until levitation_force >= 10
+
   levitation_force += 1
+
+  puts "Wingardium Leviosa"
+
   
-end
+  end
 end
 
